428d69395a98424b7c2657eb2fe12b8dddd0aa28
Zach Ayers <ayers.dev@icloud.com>
2020-11-25 09:48:29 -0600

    init

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Receiver
+    {
+        static void Main(string[] args)
+        {
+            // Create a Rabbit MQ Connection Factory
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "user",
+                Password = "0okmNJI9",
+                Port = AmqpTcpEndpoint.UseDefaultPort
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                    // Setup the consumer
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine("Received message: {0}...", message);
+                    };
+
+                    channel.BasicConsume("BasicTest", true, consumer);
+
+                    Console.WriteLine("Press any key to exit the Receiver App...");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Sender
+    {
+        public static void Main(string[] args)
+        {
+            // Create a Rabbit MQ Connection Factory
+            var factory = new ConnectionFactory() {
+                HostName = "localhost",
+                UserName = "user",
+                Password = "0okmNJI9",
+                Port = AmqpTcpEndpoint.UseDefaultPort
+            };
+
+            // Create the RabbitMQ Connection & Channel
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                // Create a basic message
+                string message = "Getting started with .NET Core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("Send message {0}...", message);
+            }
+
+            Console.WriteLine("Press any key to exit the Sender App...");
+            Console.ReadLine();
+        }
+    }
+}
